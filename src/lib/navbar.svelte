c1f921671b3f49d0cb7e5ad1683a700fe02e27dc
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-09-20 17:10:15 +0300

    navbar

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,34 @@
+
+<script>
+      import { page } from '$app/stores';
+
+      var currentUrl = $derived($page.url.pathname)
+
+</script>
+
+<div class="w-full  gap-5 text-center">
+	<div class="bg-surface-500/30 p-4 grid grid-cols-4">
+        <div class:active={currentUrl == "/quranSound"} >
+            <i class="fa-solid fa-headphones"></i> <br> <span>القراء</span>
+        </div>
+        
+        <div class:active={currentUrl == "/quran"} >
+            <i class="fa-solid fa-book-open"></i> <br>  <span>القران الكريم</span>
+        </div>
+
+      <div class:active={currentUrl == "/random"} > 
+        <i class="fa-solid fa-shuffle"></i> <br> <span>اية عشوائية</span>
+        </div>
+
+        <div class:active={currentUrl == "/sebha"} >
+            <i class="fa-solid fa-list-check"></i> <br> 		<span>سبحة</span>
+        </div>
+    </div>
+</div>
+					
+
+<style>
+     .active {
+    @apply bg-slate-800 rounded-xl ;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
 	   let { children } = $props();
 	   import '../app.postcss';
+	   import Navbar from '../lib/navbar.svelte';
 </script>
 
 {@render children()}
+
+<Navbar/>
